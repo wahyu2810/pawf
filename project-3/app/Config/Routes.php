4bcd58e91a183426eb7a812e7ce65cccd2464413
WNR28 <wahyunoerrahmat980@gmail.com>
2026-04-22 20:04:23 +0700

    update routes

--- project-3/app/Config/Routes.php
+++ project-3/app/Config/Routes.php
@@ -5,5 +5,32 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// =======================
+// HALAMAN USER (POST)
+// =======================
+
+// Homepage → tampilkan semua post
 $routes->get('/', 'Post::index');
+
+// Detail post berdasarkan slug
 $routes->get('/post/(:segment)', 'Post::detail/$1');
+
+
+// =======================
+// HALAMAN ADMIN (CRUD)
+// =======================
+
+// Dashboard admin
+$routes->get('/admin', 'Admin::index');
+
+// Create
+$routes->get('/admin/create', 'Admin::create');
+$routes->post('/admin/store', 'Admin::store');
+
+// Edit
+$routes->get('/admin/edit/(:num)', 'Admin::edit/$1');
+$routes->post('/admin/update/(:num)', 'Admin::update/$1');
+
+// Delete
+$routes->get('/admin/delete/(:num)', 'Admin::delete/$1');
