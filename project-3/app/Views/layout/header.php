fc6900dc19e560933d57365439a50e6e1f5c294d
WNR28 <wahyunoerrahmat980@gmail.com>
2026-04-22 20:50:04 +0700

    Menambahkan folder layout untuk memberikan layout ke semua halaman blog saya

---
+++ project-3/app/Views/layout/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog Wahyu</title>
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Blog Wahyu</a>
+
+        <div>
+            <a href="/" class="btn btn-outline-light btn-sm">Post</a>
+            <a href="/admin" class="btn btn-warning btn-sm">Admin</a>
+        </div>
+    </div>
+</nav>
+
+<div class="container mt-4"></div>
