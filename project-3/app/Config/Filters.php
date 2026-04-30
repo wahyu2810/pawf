239fc379b92bc5a1dca3e23d2d9cd8b0121eec57
WNR28 <wahyunoerrahmat980@gmail.com>
2026-04-30 18:20:08 +0700

    Add login and registrationusing mythAuth

--- project-3/app/Config/Filters.php
+++ project-3/app/Config/Filters.php
@@ -12,6 +12,10 @@ use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\PageCache;
 use CodeIgniter\Filters\PerformanceMetrics;
 use CodeIgniter\Filters\SecureHeaders;
+use Myth\Auth\Filters\LoginFilter; # add
+use Myth\Auth\Filters\PermissionFilter; # add
+use Myth\Auth\Filters\RoleFilter; # add
+
 
 class Filters extends BaseFilters
 {
@@ -34,6 +38,9 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'login'         => \Myth\Auth\Filters\LoginFilter::class,
+        'role'          => \Myth\Auth\Filters\RoleFilter::class,
+        'permission'    => \Myth\Auth\Filters\PermissionFilter::class,
     ];
 
     /**
@@ -106,5 +113,7 @@ class Filters extends BaseFilters
      *
      * @var array<string, array<string, list<string>>>
      */
-    public array $filters = [];
+    public array $filters = [
+	'login' => ['before' => ['admin/index', 'admin/index/*']],
+    ];
 }
